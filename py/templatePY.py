35e2dfa36dcbcca76dec6afb55e0a4d46c5fd2d3
HieuuuLeee <hieulevungtau@gmail.com>
2019-12-11 16:00:04 +0700

    folder config & add py folder

---
+++ py/templatePY.py
@@ -0,0 +1,16 @@
+import sys
+
+# like the freopen in cpp
+sys.stdin = open('in.txt','r')
+sys.stdout = open('out.txt','w')
+
+# input value
+x = input()
+x = bin(x)[0:]
+print(x)
+#output: 0b110100
+
+ls = len(x)
+print(len(x)[2:])
+#output: 6
+
