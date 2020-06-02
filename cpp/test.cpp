f2dcf36723c9bd9bb18d49877009940d7054351b
HieuuuLeee <hieulevungtau@gmail.com>
2020-06-02 10:08:59 +0700

    add graph folder

---
+++ cpp/test.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define _CRT_SECURE_NO_DEPRECATE
+using namespace std;
+
+int main(){
+    freopen("in.txt","r",stdin);
+    // freopen("out.txt","w",stdout);
+    if( stream == NULL )
+        fprintf( stdout, "error on freopen\n" );
+    else
+    {
+        fprintf( stdout, "successfully reassigned\n" ); fflush( stdout );
+        fprintf( stream, "This will go to the file 'freopen.out'\n" );
+        fclose( stream );
+    }
+    int a;
+    // cin >> a;
+    cout << a;
+    cout << "hh";
+}
