ac34a019de156184b86e88c7ac1eda014afe7f6d
HieuuuLeee <hieulevungtau@gmail.com>
2020-06-04 16:46:01 +0700

    update anansisCobWeb.cpp

--- cpp/graph/dsu/anansiCobWeb.cpp
+++ cpp/graph/dsu/anansiCobWeb.cpp
@@ -4,13 +4,13 @@ using namespace std;
 #define fi first
 #define se second
 #define pb push_back
+#define pf push_front
 #define pp push
 #define et empty
 #define mp make_pair
 #define count_array(a,b,n,indx) {int fiIndx=-1; for(int iiiii=0;iiiii<n;iiiii++) if(iiiii==n-1||(a[iiiii]!=a[iiiii+1])) {b[indx++]=iiiii-fiIndx; fiIndx=iiiii;}}
 #define prefix_arr(a,b,n) {b[0]=a[0]; for(int iiiii=1;iiiii<n,iiiii++) b[iiiii]=b[iiiii-1]+a[iiiii];}
 #define suffix_arr(a,b,n) {b[n-1]=a[n-1]; for(int iiiii=n-2;iiiii>=0,iiiii--) b[iiiii]=b[iiiii+1]+a[iiiii];}
-
 
 #ifdef ONLINE_JUDGE
 #define CURTIME()         ;
@@ -27,6 +27,8 @@ using namespace std;
 #endif
 #define For(i,a,b) for (int i=a;i<b;i++)
 #define Fod(i,b,a) for (int i=b;i>a;i--)
+#define ForV(v) for (auto it = v.begin(); it!=v.end(); ++it)
+
 typedef int64_t ll;
 typedef uint64_t ull;
 #define brln cout << "\n";
@@ -36,61 +38,44 @@ typedef uint64_t ull;
 #define bit(x, i)           (((x) >> (i)) & 1)
 #define bitcount(n)         __builtin_popcountll(n)
 
-const int M = 1e5 + 5;
+void union_set(int u, int v){
+
+}
+
+void findConnectedComponent(){
+
+}
+
+void check_ConnComp(int u, int v, int res){
+
+}
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
   CURTIME();
   INFILE("../in.txt");
   OUFILE("../out.txt");
-  int n, m;
+  int n, m, q, res;
   cin >> n >> m;
-  vector<int> par(n), rnk(n, 0);
-  iota(begin(par), end(par), 0);
-  vector<pair<int, int>> thread(m);
-  for (auto& [i, j] : thread) cin >> i >> j, --i, --j;
-  int Q;
-  cin >> Q;
-  vector<int> q(Q);
-  bitset<M> has;
-  for (int& i : q) {
-    cin >> i; --i;
-    has[i] = true;
-  }
-  function<int(int)> root = [&](int v) {
-    return v == par[v] ? v : par[v] = root(par[v]);
-  };
-  auto uniset = [&](int a, int b) {
-    a = root(a);
-    b = root(b);
-    if (a == b)
-      return;
-    if (rnk[a] < rnk[b])
-      swap(a, b);
-    par[b] = a;
-    rnk[a] += rnk[a] == rnk[b];
-  };
-  for (int i = 0; i < m; i++) {
-    if (!has[i]) {
-      uniset(thread[i].first, thread[i].second);
+  pair<int, int> a[n+1];
+  For(i,0,m) cin>>a[i].fi>>a[i].se;
+  cin>>q;
+  int b[q+1];
+  map<int, int> check;
+  For(i,0,q){
+    cin>>b[i];
+    check[b[i]] = 1;
+  } 
+  For(i,0,m){
+    if(!check[i]){
+      union_set(a[i].fi, a[i].se);
     }
   }
-  bitset<M> cc;
-  for (int i = 0; i < n; i++) {
-    cc[root(i)] = true;
+  res = findConnectedComponent();
+  list<int> results; results.pf(res);
+  Fod(i,q-1,0){
+    check_ConnComp(a[b[i]].fi, a[b[i]].fi, res);
+    results.pf(res);
   }
-  int ncc = cc.count();
-  int minus = 0;
-  list<int> ans;
-  reverse(begin(q), end(q));
-  for (int i : q) {
-    ans.push_front(ncc);
-    if (root(thread[i].first) != root(thread[i].second))
-      ncc -= 1;
-    uniset(thread[i].first, thread[i].second);
-  }
-  for (int i : ans)
-    cout << i << ' ';
+  ForV(v) cout << *it;
   return 0;
 }
