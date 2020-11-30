5088954f1cc23a710682cdd72d2bca5618a1870f
HieuuuLeee <hieulevungtau@gmail.com>
2020-11-30 20:15:25 +0700

    update 30/11/2020

---
+++ string/M_TextDocumentAnalysis.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/723/B
+#include<bits/stdc++.h>
+using namespace std;
+#define INFILE(name)      freopen(name, "r", stdin)
+#define OUFILE(name)      freopen(name, "w", stdout)
+int main(){
+	INFILE("../in.txt");
+    OUFILE("../out.txt");
+    string s = "_Hello_Vasya";
+    vector<string> ss;
+   	int pre = 0;
+   	for(int j=pre+1)
+}
